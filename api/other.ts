7d85031f850562296b007cfc4d88e4a2655a97b8
keitakn <keita.koga.work@gmail.com>
2022-11-17 22:28:25 +0900

    :sparkles: 別のAPI Routeを追加

---
+++ api/other.ts
@@ -0,0 +1,16 @@
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async (req: Request) => {
+  const body = {
+    message: 'other edge api!!',
+  };
+
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+}
