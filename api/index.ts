df2081721f4a6924baec38b75fa2d8a48f064b89
keitakn <keita.koga.work@gmail.com>
2022-11-19 19:28:48 +0900

    :sparkles: honoの動作確認用コードを追加

---
+++ api/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/api/hello', (c) => {
+  return c.json({ message: 'Hello Hono!' });
+});
+
+export default async (req: Request) => {
+  return app.request(req);
+}
+
+export const config = {
+  runtime: 'experimental-edge',
+}
