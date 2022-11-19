f1ed2c34011b407f80006366f1423507df5cecd2
keitakn <keita.koga.work@gmail.com>
2022-11-19 20:14:15 +0900

    :fire: [...slug].ts 以外の全てのAPI Routeを削除

--- api/oidc/token.ts
+++
@@ -1,9 +0,0 @@
-import { issueAccessTokenHandler } from '../../src/issueAccessTokenHandler';
-
-export const config = {
-  runtime: 'experimental-edge',
-}
-
-export default async (req: Request) => {
-  return issueAccessTokenHandler(req);
-}
